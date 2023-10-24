ac6a7ed845bb28a2f527a7b20d2b4c0f817effed
elestrago <elestrago63@gmail.com>
2023-10-25 01:11:50 +0200

    [upm] Remove dependency on Object for ISerializedFluent extensions

--- Editor/Utils/SerializedObjectFluentExtensions.cs
+++ Editor/Utils/SerializedObjectFluentExtensions.cs
@@ -2,48 +2,44 @@ using System;
 using System.Collections;
 using System.Linq.Expressions;
 using Playdarium.Serializer.Runtime.Fluent;
-using Object = UnityEngine.Object;
 
 namespace Playdarium.Serializer.Utils
 {
 	public static class SerializedObjectFluentExtensions
 	{
 		public static ISerializedFluent<T> Set<T>(
-			this ISerializedFluent<T> so,
+			this ISerializedFluent<T> serialized,
 			Expression<Func<T, IEnumerable>> expression,
 			IEnumerable obj
 		)
-			where T : Object
-			=> so.Set(expression.GetPropertyName(), obj);
+			=> serialized.Set(expression.GetPropertyName(), obj);
 
 		public static ISerializedFluent<T> Set<T, TProperty>(
-			this ISerializedFluent<T> so,
+			this ISerializedFluent<T> serialized,
 			Expression<Func<T, TProperty>> expression,
 			TProperty obj
 		)
-			where T : Object
 		{
 			var propertyName = expression.GetPropertyName();
 			if (obj is IEnumerable enumerable)
-				return so.Set(propertyName, enumerable);
+				return serialized.Set(propertyName, enumerable);
 
-			return so.Set(propertyName, obj);
+			return serialized.Set(propertyName, obj);
 		}
 
 		public static ISerializedFluent<TSerialized> Set<TSerialized, TSerializable>(
-			this ISerializedFluent<TSerialized> so,
+			this ISerializedFluent<TSerialized> serialized,
 			string config,
 			TSerializable obj
 		)
-			where TSerialized : Object
 		{
-			var property = so.GetProperty(config);
+			var property = serialized.GetProperty(config);
 			if (obj is IEnumerable enumerable)
 				SerializedPropertySerializer.SerializeArrayProperty(enumerable, property);
 			else
 				SerializedPropertySerializer.SerializeClassType(obj, property);
 
-			return so;
+			return serialized;
 		}
 	}
 }
