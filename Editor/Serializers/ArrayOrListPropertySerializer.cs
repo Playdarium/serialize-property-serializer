0a4ba4d35bb1f90b29c31c315fe3ef5a9a6fd314
elestrago <elestrago63@gmail.com>
2023-10-25 09:02:54 +0200

    [upm] Fix work SerializedPropertySerializer

--- Editor/Serializers/ArrayOrListPropertySerializer.cs
+++ Editor/Serializers/ArrayOrListPropertySerializer.cs
@@ -9,7 +9,17 @@ namespace Playdarium.Serializer.Serializers
 	{
 		public bool CanSerialize(Type type) => type.IsArray || type.HasInterface<IList>();
 
-		public void Serialize(object value, SerializedProperty property) 
-			=> SerializedPropertySerializer.SerializeArrayProperty((IEnumerable) value, property);
+		public void Serialize(object value, SerializedProperty property)
+		{
+			var array = (IEnumerable)value;
+			var currentIndex = 0;
+			property.arraySize = 0;
+			foreach (var item in array)
+			{
+				property.arraySize++;
+				var element = property.GetArrayElementAtIndex(currentIndex++);
+				SerializedPropertySerializer.SerializeValueProperty(item, element);
+			}
+		}
 	}
 }
